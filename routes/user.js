9094f93d81e912b21fcac8bac76fc05d69617482
anjuvinay <aanjups88@gmail.com>
2022-12-22 20:55:04 +0530

    order placed, cod is finished, online payment pending

--- routes/user.js
+++ routes/user.js
@@ -68,8 +68,9 @@ router.get('/logout',(req,res)=>{
 
 router.get('/cart',verifyLogin,async(req,res)=>{
   let products=await userHelpers.getCartProducts(req.session.user._id)
+  let totalValue=await userHelpers.getTotalAmount(req.session.user._id)
  
-  res.render('user/cart',{products,user:req.session.user})
+  res.render('user/cart',{products,user:req.session.user, totalValue})
 })
 
 router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{  
@@ -79,7 +80,8 @@ router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
 })
 
 router.post('/change-product-quantity',(req,res,next)=>{
-  userHelpers.changeProductQuantity(req.body).then((response)=>{
+  userHelpers.changeProductQuantity(req.body).then(async(response)=>{
+    response.total=await userHelpers.getTotalAmount(req.body.user)
     res.json(response)    
   })
 })
@@ -93,7 +95,17 @@ router.post('/remove-item',(req,res)=>{
 
 router.get('/place-order',verifyLogin, async(req,res)=>{
   let total=await userHelpers.getTotalAmount(req.session.user._id)
-  res.render('user/place-order',{total})
+  res.render('user/place-order',{total,user:req.session.user})
+})
+
+router.post('/place-order',async(req,res)=>{
+  let products=await userHelpers.getCartProductList(req.body.userId)
+  let totalPrice=await userHelpers.getTotalAmount(req.body.userId)
+  userHelpers.placeOrder(req.body, products, totalPrice).then((response)=>{
+    res.json({status:true})
+
+  })
+  console.log(req.body)
 })
 
 
