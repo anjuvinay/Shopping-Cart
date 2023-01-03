923e62bd0e0bf30f58515c90a2b9a290d6155859
anjuvinay <aanjups88@gmail.com>
2023-01-04 00:35:43 +0530

    online payment completed

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -5,6 +5,7 @@ const { USER_COLLECTION } = require('../config/collections')
 const { response } = require('../app')
 var objectId = require('mongodb').ObjectID
 const Razorpay=require('razorpay')
+const { resolve } = require('path')
 
 var instance = new Razorpay({
     key_id: 'rzp_test_IvGwMBpENteAE2',
@@ -304,16 +305,48 @@ getOrderProducts:(orderId)=>{
 generateRazorpay:(orderId,total)=>{
     return new Promise((resolve,reject)=>{
         var options={
-           amount:total,
+           amount:total*100,
            currency:"INR" ,
-           receipt:orderId  
+           receipt:""+orderId
         }
         instance.orders.create(options,function(err,order){
-            console.log("New order :",order)
-            resolve(order)
+            if(err){
+                console.log(err)
+            }else{
+                console.log("New order :",order)
+                resolve(order)
+            }
+           
         })
        
     })
+},
+verifyPayment:(details)=>{
+    return new Promise((resolve,reject)=>{
+        const crypto=require('crypto');
+        let hmac=crypto.createHmac('sha256', 'tH4vJwFji3WLnDms9bT9sPyL')
+        hmac.update(details['payment[razorpay_order_id]']+'|'+details['payment[razorpay_payment_id]'])
+        hmac=hmac.digest('hex')
+        if(hmac==details['payment[razorpay_signature]']){
+            resolve()
+        }else{
+            reject()
+        }
+    })
+},
+changePaymentStatus:(orderId)=>{
+    return new Promise((resolve,reject)=>{
+
+    db.get().collection(collection.ORDER_COLLECTION).updateOne({_id:objectId(orderId)},
+    {
+        $set:{
+            status:'placed'
+        }
+
+    }).then(()=>{
+        resolve()
+    })
+})
 }
    
 }
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
+const { getOrderProducts } = require('../helpers/user-helpers');
 const userHelpers=require('../helpers/user-helpers')
 
 const verifyLogin = (req, res, next)=>{
@@ -68,7 +69,11 @@ router.get('/logout',(req,res)=>{
 
 router.get('/cart',verifyLogin,async(req,res)=>{
   let products=await userHelpers.getCartProducts(req.session.user._id)
-  let totalValue=await userHelpers.getTotalAmount(req.session.user._id)
+  let totalValue=0
+  if(products.length>0){
+    let totalValue=await userHelpers.getTotalAmount(req.session.user._id)
+  }
+ 
  
   res.render('user/cart',{products,user:req.session.user, totalValue})
 })
@@ -102,8 +107,8 @@ router.post('/place-order',async(req,res)=>{
   let products=await userHelpers.getCartProductList(req.body.userId)
   let totalPrice=await userHelpers.getTotalAmount(req.body.userId)
   userHelpers.placeOrder(req.body, products, totalPrice).then((orderId)=>{
-    if(req.body['payment-method']=='COD'){
-      res.json({status:true})
+    if(req.body['payment-method']==='COD'){
+      res.json({codSuccess:true})
     }
     else{
       userHelpers.generateRazorpay(orderId,totalPrice).then((response)=>{
@@ -128,6 +133,17 @@ router.get('/orders',async(req,res)=>{
 router.get('/view-order-products/:id',async(req,res)=>{
   let products=await userHelpers.getOrderProducts(req.params.id)
   res.render('user/view-order-products',{user:req.session.user,products})
+})
+
+router.post('/verify-payment',(req,res)=>{
+  console.log(req.body)
+  userHelpers.verifyPayment(req.body).then(()=>{
+    userHelpers.changePaymentStatus(req.body['order[receipt]']).then(()=>{
+      res.json({status:true})
+    })
+  }).catch((err)=>{
+    res.json({status:false,errMsg:''})
+  })
 })
 
 
