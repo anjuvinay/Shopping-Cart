1c8f40e80686b6ef21c89d7f411aa03cd786e7c1
anjuvinay <aanjups88@gmail.com>
2023-01-10 23:06:53 +0530

    final commit

--- routes/admin.js
+++ routes/admin.js
@@ -5,26 +5,30 @@ const productHelpers = require('../helpers/product-helpers');
 var productHelper=require('../helpers/product-helpers')
 const adminHelpers=require('../helpers/admin-helpers')
 
-const verifyLogin = (req, res, next)=>{
+const verifyLogiin = (req, res, next)=>{
   if(req.session.admin){
     next()
   }else{
-    res.redirect('admin/log-in')
+    res.redirect('/admin/')
   }
 }
 
-/* GET users listing. */
-router.get('/',verifyLogin, function(req, res, next) {
+
+
+router.get('/', function(req, res){
   let admin=req.session.admin
-  productHelpers.getAllProducts().then((products)=>{
-    console.log(products)
-    res.render('admin/view-products',{admin:true, products})
-  })
- 
-});
+  if(req.session.admin){   
+  res.redirect('admin/view-products',{Admin:req.session.admin,admin,products})
+}else{
+  res.render('admin/log-in',{Admin:req.session.admin,admin:true})
+  
+}
+
+})
 
 router.get('/add-product', function(req, res) {
-  res.render('admin/add-product')
+  let admin=req.session.admin
+  res.render('admin/add-product',{Admin:req.session.admin,admin})
 })
 
 router.post('/add-product',(req,res)=>{
@@ -33,13 +37,11 @@ router.post('/add-product',(req,res)=>{
 
   productHelpers.addProduct(req.body,(insertedId)=>{
     let image=req.files.Image
-    
+       
     image.mv('../SHOPPING CART/public/product-images/'+insertedId+'.jpg',(err,done)=>{
+      
       if(!err){
-        productHelpers.getAllProducts().then((products)=>{
-          console.log(products)
-          res.render('admin/view-products',{admin:true, products})
-        })
+        res.redirect('/admin/view-products')
       }else{
         console.log(err)
       }
@@ -48,14 +50,14 @@ router.post('/add-product',(req,res)=>{
   })
 })
 
+
+ 
+
 router.get('/delete-product/:id',(req,res)=>{
   let proId=req.params.id
   console.log(proId)
   productHelpers.deleteProduct(proId).then((response)=>{
-    productHelpers.getAllProducts().then((products)=>{
-      console.log(products)
-      res.render('admin/view-products',{admin:true, products})
-    })
+    res.redirect('/admin/view-products')
   })
  
 })
@@ -68,9 +70,7 @@ router.get('/edit-product/:id', async(req,res)=>{
 router.post('/edit-product/:id',(req,res)=>{
   let insertedId=req.params.id
   productHelpers.updateProduct(req.params.id, req.body).then(()=>{
-    productHelpers.getAllProducts().then((products)=>{
-      res.render('admin/view-products',{admin:true, products})
-    })
+    res.redirect('/admin/view-products')
 
     if(req.files.Image){
       let image=req.files.Image
@@ -80,15 +80,7 @@ router.post('/edit-product/:id',(req,res)=>{
   
 })
 
-router.get('/log-in', function(req, res){
-  if(req.session.admin){   
-  res.render('admin/log-in',{admin:true})
-}else{
-  res.render('admin/log-in',{"loginErr":req.session.adminLoginErr,admin:true})
-  req.session.adminLoginErr=false
-}
 
-})
 
 router.get('/sign-up', function(req, res){
 res.render('admin/sign-up',{admin:true})
@@ -107,17 +99,16 @@ router.post('/sign-up', function(req, res){
  })
 
  router.post('/log-in',(req, res)=>{
+  
   adminHelpers.doLog_in(req.body).then((response)=>{  
     if(response.status){ 
       req.session.admin=response.admin
       req.session.admin.loggedIn=true
-  productHelpers.getAllProducts().then((products)=>{
-    console.log(products)
-    res.render('admin/view-products',{admin:true, products})
-  })
+      let  admin=req.session.admin;
+  res.redirect('/admin/view-products')
     }else{
       req.session.adminLoginErr="Invalid username or Password"
-      res.redirect('admin/log-in')
+      res.redirect('/admin/')
     }
    
 })
@@ -126,10 +117,10 @@ router.post('/sign-up', function(req, res){
 router.get('/log-out',(req,res)=>{
   req.session.admin=null
   req.session.adminLoggedIn=false
-  res.redirect('/admin/log-in')
+  res.redirect('/admin/')
 })
 
-router.get('/all-orders',(req,res)=>{
+router.get('/all-orders',verifyLogiin,(req,res)=>{
   let  admin=req.session.admin;
   
   productHelpers.getAllOrders().then((orders) => {
@@ -138,7 +129,7 @@ router.get('/all-orders',(req,res)=>{
   })
 })
 
-router.get('/all-users',(req,res)=>{
+router.get('/all-users',verifyLogiin,(req,res)=>{
   let  admin=req.session.admin;
   
   productHelpers.getAllUsers().then((users) => {
@@ -147,11 +138,11 @@ router.get('/all-users',(req,res)=>{
   })
 })
 
-router.get('/view-products', function(req, res, next) {
+router.get('/view-products',verifyLogiin, function(req, res, next) {
   let admin=req.session.admin
   productHelpers.getAllProducts().then((products)=>{
     console.log(products)
-    res.render('admin/view-products',{Admin:req.session.admin,admin, products})
+    res.render('admin/view-products',{Admin:req.session.admin,admin,products})
   })
  
 });
